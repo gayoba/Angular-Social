98ce3480fe0da54902293f25f731b40a57be8721
gayoba <johnmeriam2@gmail.com>
2018-03-07 16:42:21 +0800

    add task module

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
     firebaseConfig: {
         apiKey: 'AIzaSyBf562OQpawlCGF5m9vjZ1GZOWEdbdhvoI',
         authDomain: 'AngularSocial.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
+        databaseURL: 'https://AngularSocial.firebaseio.com',
         projectId: 'angularsocial-3dd1e',
         storageBucket: 'AngularSocial.appspot.com',
         messagingSenderId: '801650646520'
